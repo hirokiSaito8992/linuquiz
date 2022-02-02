c8c9f1dbc117d134f9bfbb0c01626c656175e34f
齋藤啓基 <hiroki@hirokisaito8992.local>
2022-02-03 02:10:10 +0900

    少し修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,20 @@
 @section('content')
 @include('layouts.error_list')
 
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card my-5">
+                <div class="card-body text-left mx-5">
+                    <h3 class="mt-2">{{ $users->name }}さんが投稿した問題</h3>
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 @endsection
